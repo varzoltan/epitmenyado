12f13f1b05ec54debe73e8fddc9f1706cb3852c4
Zoltán Varga <varzoltan@tanarium.hu>
2022-09-03 12:26:00 +0200

    12:25

--- epitmenyado/Program.cs
+++ epitmenyado/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace epitmenyado
 {
@@ -13,16 +14,27 @@ namespace epitmenyado
         public string hazszam { get; set; }
         public string adosav { get; set; }
         public int alapterulet { get; set; }
-        public Adat(string sor)
+        public string utca_egybe { get { return utca + " utca " + hazszam; } }
+        public Adat(string sor)//konstruktor
         {
             string[] db = sor.Split();
-
+            adoszam = db[0];
+            utca = db[1];
+            hazszam = db[2];
+            adosav = db[3];
+            alapterulet = int.Parse(db[4]);
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            //1.feladat: Beolvasás
+            List<Adat> lista = new List<Adat>();
+            foreach (var i in File.ReadAllLines("utca.txt").Skip(1))
+            {
+                lista.Add(new Adat(i));
+            }
         }
     }
 }
