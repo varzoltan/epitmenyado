2a7888cd9f6edf825b0e5980c0e3612f1343259f
Zoltán Varga <varzoltan@tanarium.hu>
2022-09-03 12:10:38 +0200

    Add project files.

---
+++ epitmenyado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epitmenyado
+{
+    class Adat
+    {
+        public string adoszam { get; set; }
+        public string utca { get; set; }
+        public string hazszam { get; set; }
+        public string adosav { get; set; }
+        public int alapterulet { get; set; }
+        public Adat(string sor)
+        {
+            string[] db = sor.Split();
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
