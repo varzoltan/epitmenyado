8b68b2a323df81829b2d3172c60e7082034483b9
Zoltán Varga <varzoltan@tanarium.hu>
2022-09-09 17:24:07 +0200

    2022.09.09 17:23

--- epitmenyado/Program.cs
+++ epitmenyado/Program.cs
@@ -16,6 +16,7 @@ namespace epitmenyado
         public int alapterulet { get; set; }
         public string utca_egybe { get { return utca + " utca " + hazszam; } }
         public Adat(string sor)//konstruktor
+
         {
             string[] db = sor.Split();
             adoszam = db[0];
@@ -29,6 +30,7 @@ namespace epitmenyado
     {
         static void Main(string[] args)
         {
+            
             //1.feladat: Beolvasás
             List<Adat> lista = new List<Adat>();
             foreach (var i in File.ReadAllLines("utca.txt").Skip(1))
@@ -36,6 +38,26 @@ namespace epitmenyado
                 lista.Add(new Adat(i));
             }
 
+            //2.feladat
+            Console.WriteLine($"2. feladat. A mintában {lista.Count} telek szerepel.");
+
+            //3.feladat
+            Console.Write("3. feladat. Egy tulajdonos adószáma: ");
+            string adoszam = Console.ReadLine();
+            bool volt = true;
+            foreach (var i in lista)
+            {
+                if (i.adoszam == adoszam)
+                {
+                    Console.WriteLine($"{i.utca_egybe}");
+                    volt = false;
+                }
+            }
+            if (volt == true)
+            {
+                Console.WriteLine("Nem szerepel az adatállományban");
+            }
+            
             Console.ReadKey();
         }
     }
